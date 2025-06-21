53c6056d8274792d07786500c319bd41c5b8b668
Diogo-Nandes <120397281+Diogo-Nandes@users.noreply.github.com>
2025-06-21 16:43:48 +0100

    Update __init__.py

--- gestao_aluguer/__init__.py
+++ gestao_aluguer/__init__.py
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 from .gestao_aluguer import GestaoAluguerAutomoveis, GestorVeiculos, GestorClientes
-=======
-from .gestao_aluguer import GestaoAluguerAutomoveis, GestorVeiculos, GestorClientes
->>>>>>> 8396a61 (Primeiro commit)
